059cef670a53522ddcbed6aaad624a5f7e614e0f
TBye101 <ThomasBye1@gmail.com>
2021-01-15 15:44:20 -0600

    Basic listen command done to listen to everything

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 
     https://crates.io/crates/cargo-inspect
 
+    https://crates.io/crates/indicatif
+
+
     Testing:
     https://crates.io/crates/static_assertions
     https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
+    https://crates.io/crates/assert_cmd
 
     db:
-    https://github.com/tikv/tikv
+    https://github.com/spacejam/sled
 
     performance:
     https://github.com/async-rs/async-std
@@ -24,44 +28,103 @@
 
 extern crate etherparse;
 
+use std::io::Read;
 use std::sync::Arc;
 use std::thread::JoinHandle;
+
 use pcap::Device;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 use log_t::logging_abstraction::Logger;
 use log_t::logging_implementations::FileLogger;
 
 fn main() {
-    let log = FileLogger::new_from_static_string("eavesdropping_log.txt").ok().unwrap();
-    let shared_log = Arc::new(log);
+    let listen_command: Box<cliargs_t::Command> = Box::new(ListenCommand {});
+    let mut commands = vec![
+        listen_command
+    ];
+    let commander = cliargs_t::Commander::new(&mut commands);
 
-    let mut listener_threads: Vec<JoinHandle<()>> = Vec::new();
-    for device in Device::list().unwrap() {
-        shared_log.write(format!("Listening to device: {:?}", device));
-        println!("Listening to device: {:?}", device);
-        let shared_log_clone = shared_log.clone();
-        let join_handle = std::thread::spawn(move || {
-            let device_name = device.name.clone();
-            let cap_wrapped = device.open();
-            if cap_wrapped.is_ok() {
-                let mut cap = cap_wrapped.unwrap();
-                let mut savefile = cap.savefile(format!("capture_device_{}.pcap", device_name)).unwrap();
-                let mut count = 0;
-    
-                while let Ok(packet) = cap.next() {
-                    count += 1;
-                    shared_log_clone.write(format!("Number of packets captured: {:?} captured by: {:?}", count, device_name));
-                    savefile.write(&packet);
-                }
-            }
-            else {
-                shared_log_clone.write(format!("Couldn't listen to {:?}", device_name));
-            }
-        });
-        listener_threads.push(join_handle);
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
     }
 
-    while true {}
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                commander.handle_input(line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
+}
+
+struct ListenCommand {}
+
+impl cliargs_t::Command for ListenCommand {
+    
+    fn execute_command(&self, _: std::collections::HashMap<std::string::String, std::string::String>) {
+        let capture_manager_thread = std::thread::spawn(move || {
+            let log = FileLogger::new_from_static_string("eavesdropping_log.txt").ok().unwrap();
+            let shared_log = Arc::new(log);
+        
+            let mut listener_threads: Vec<JoinHandle<()>> = Vec::new();
+            for device in Device::list().unwrap() {
+                shared_log.write(format!("Listening to device: {:?}", device));
+                println!("Listening to device: {:?}", device);
+                let shared_log_clone = shared_log.clone();
+                let join_handle = std::thread::spawn(move || {
+                    let device_name = device.name.clone();
+                    let cap_wrapped = device.open();
+                    if cap_wrapped.is_ok() {
+                        let mut cap = cap_wrapped.unwrap();
+                        let mut savefile = cap.savefile(format!("capture_device_{}.pcap", device_name)).unwrap();
+                        let mut count = 0;
+            
+                        while let Ok(packet) = cap.next() {
+                            count += 1;
+                            shared_log_clone.write(format!("Number of packets captured: {:?} captured by: {:?}", count, device_name));
+                            savefile.write(&packet);
+                        }
+                    }
+                    else {
+                        shared_log_clone.write(format!("Couldn't listen to {:?}", device_name));
+                    }
+                });
+                listener_threads.push(join_handle);
+            }
+        });
+    }
+
+    fn get_information(&self) -> cliargs_t::CommandInformation { 
+        return cliargs_t::CommandInformation {
+            command_name: "listen",
+            command_help: "Captures packet traffic",
+            flags: vec![
+                cliargs_t::Flag {
+                    identifier: "a",
+                    flag_help: "Listen to all available devices/networks",
+                    required: false
+                }
+            ]
+        }
+    }
 }
 
     // let mut devices = Device::list().unwrap();
