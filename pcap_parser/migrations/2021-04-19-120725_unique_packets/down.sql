cb0f5b287943f71ab8ed8f064e0a04fc07a6dfff
Thomas Bye <ThomasBye1@outlook.com>
2021-04-28 11:29:10 +0000

    Added in unique hashing of the captured data as part of the unique index to prevent duplicates

--- pcap_parser/migrations/2021-04-19-120725_unique_packets/down.sql
+++ pcap_parser/migrations/2021-04-19-120725_unique_packets/down.sql
@@ -1,3 +1,6 @@
 -- This file should undo anything in `up.sql`
 
-DROP INDEX IF EXISTS deduplication_index
+DROP INDEX IF EXISTS deduplication_index;
+
+ALTER TABLE packets_pcap_parser
+DROP COLUMN IF EXISTS payload_hash
--- pcap_parser/migrations/2021-04-19-120725_unique_packets/up.sql
+++ pcap_parser/migrations/2021-04-19-120725_unique_packets/up.sql
@@ -1,3 +1,5 @@
 -- Your SQL goes here
+ALTER TABLE packets_pcap_parser
+ADD COLUMN payload_hash varchar(16) NOT NULL;
 
-CREATE UNIQUE INDEX deduplication_index ON packets_pcap_parser(payload, captured_timestamp)
+CREATE UNIQUE INDEX deduplication_index ON packets_pcap_parser(payload_hash, captured_timestamp)
